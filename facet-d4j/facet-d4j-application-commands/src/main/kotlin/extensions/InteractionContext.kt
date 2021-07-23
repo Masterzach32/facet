fc09447cfd92c4724a69fb6cb90d1111b31f34c4
Zach Kozar <zachkozar@vt.edu>
2021-07-22 22:05:04 -0400

    Add optional ephemeral parameter to acknowledge

--- facet-d4j/facet-d4j-application-commands/src/main/kotlin/extensions/InteractionContext.kt
+++ facet-d4j/facet-d4j-application-commands/src/main/kotlin/extensions/InteractionContext.kt
@@ -15,10 +15,7 @@
 
 package io.facet.discord.appcommands.extensions
 
-import discord4j.core.spec.*
-import discord4j.discordjson.json.*
 import io.facet.discord.appcommands.*
-import io.facet.discord.dsl.*
 import io.facet.discord.extensions.*
 
 /*
@@ -34,38 +31,10 @@ import io.facet.discord.extensions.*
  * @version 6/6/2021
  */
 
-public suspend fun SlashCommandContext.acknowledge(): Unit = event.acknowledge().await()
+public suspend fun SlashCommandContext.acknowledge(ephemeral: Boolean = false): Unit =
+    if (ephemeral)
+        event.acknowledgeEphemeral().await()
+    else
+        event.acknowledge().await()
 
 public suspend fun SlashCommandContext.acknowledgeEphemeral(): Unit = event.acknowledgeEphemeral().await()
-
-@Deprecated(
-    "Use functions on interactionResponse", ReplaceWith(
-        "event.interactionResponse.createFollowupMessage(content).await()",
-        "io.facet.discord.extensions.await"
-    )
-)
-public suspend fun SlashCommandContext.createFollowupMessage(content: String): MessageData = event
-    .interactionResponse
-    .createFollowupMessage(content)
-    .await()
-
-@Deprecated(
-    "", ReplaceWith(
-        "event.interactionResponse.createFollowupMessage(template.asMultipartRequest()).await()",
-        "io.facet.discord.extensions.await"
-    )
-)
-public suspend fun SlashCommandContext.createFollowupMessage(template: EmbedCreateSpec): MessageData {
-    return event.interactionResponse.createFollowupMessage(template.asMultipartRequest()).await()
-}
-
-@Deprecated(
-    "", ReplaceWith(
-        "event.interactionResponse.createFollowupMessage(embed(buildBlock).asMultipartRequest()).await()",
-        "io.facet.discord.dsl.embed",
-        "io.facet.discord.extensions.await"
-    )
-)
-public suspend fun SlashCommandContext.createFollowupMessage(buildBlock: EmbedBuilder.() -> Unit): MessageData {
-    return event.interactionResponse.createFollowupMessage(embed(buildBlock).asMultipartRequest()).await()
-}
